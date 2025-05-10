075a48f88d997f7263ff1d9ae200a35624bdabe5
denis23x <damage.23x@gmail.com>
2025-05-10 12:06:06 +0700

    feat: integrate Radix UI components and theme support
    - Added new dependencies for enhanced UI components and theming:
      - @radix-ui/react-dialog
      - @radix-ui/react-navigation-menu
      - @radix-ui/react-slot
      - @radix-ui/react-visually-hidden
      - next-themes
    - Implemented ThemeProvider in layout for theme management
    - Created ThemeToggle component for light/dark theme switching
    - Developed responsive Navbar with navigation links
    - Added mobile-friendly menu with Sheet component
    - Refactored main page to use new Navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button';
+import { Logo } from './logo';
+import { NavMenu } from './nav-menu';
+import { NavigationSheet } from './navigation-sheet';
+import ThemeToggle from '../theme-toggle';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-background border-accent h-16 border-b">
+      <div className="mx-auto flex h-full max-w-screen-xl items-center justify-between px-4 sm:px-6">
+        <Logo />
+
+        {/* Desktop Menu */}
+        <NavMenu className="hidden md:block" />
+
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <Button variant="outline" className="hidden sm:inline-flex">
+            Sign In
+          </Button>
+          <Button className="xs:inline-flex hidden">Get Started</Button>
+
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            <NavigationSheet />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
