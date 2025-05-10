c9c6810549a3302bd0738ef55f4dccdac7f0f788
denis23x <damage.23x@gmail.com>
2025-05-10 12:08:56 +0700

    feat: add Hero component and placeholder SVG
    - Introduced a new Hero component to enhance the homepage layout with a promotional section.
    - Replaced the static "Hello World" header with the Hero component in the main page.
    - Added a placeholder SVG image for visual representation in the Hero component.
    - Created a Badge component for displaying release information within the Hero section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from '@/components/navbar/navbar';
+import Hero from '@/components/hero';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Hello World</h1>
+      <Hero />
     </div>
   );
 }
