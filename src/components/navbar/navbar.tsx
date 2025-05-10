9deee12a688f764de2201a823b1e7441bac34cdd
denis23x <damage.23x@gmail.com>
2025-05-10 13:28:22 +0700

    feat: enhance Navbar component with fixed positioning and backdrop blur effect

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-background border-accent h-16 border-b">
+    <nav className="border-accent bg-background/80 fixed top-0 left-0 z-50 h-16 w-full border-b backdrop-blur-sm backdrop-filter">
       <div className="mx-auto flex h-full max-w-screen-xl items-center justify-between px-4 sm:px-6">
         <Logo />
 
