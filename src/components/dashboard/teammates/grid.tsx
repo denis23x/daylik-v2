e1c9f3dc6c4c4e0d94744fd1aa611647a37ea620
denis23x <damage.23x@gmail.com>
2025-05-24 14:37:12 +0700

    feat(teammates, ui): integrate Radix UI accordion and collapsible components

--- src/components/dashboard/teammates/grid.tsx
+++ src/components/dashboard/teammates/grid.tsx
@@ -10,7 +10,7 @@ import { useTeammatesStore } from '@/store/useTeammatesStore';
 import { supabase } from '@/utils/supabase/client';
 import { toast } from 'sonner';
 import { Button } from '@/components/ui/button';
-import { Pencil } from 'lucide-react';
+import { Dices, Pencil, Shuffle } from 'lucide-react';
 import GridWithHoverEffect from '@/components/grid-with-hover-effect';
 
 const TeammatesGrid = () => {
@@ -37,44 +37,57 @@ const TeammatesGrid = () => {
   };
 
   return (
-    <div className="container mx-auto flex min-h-screen items-center justify-center px-4">
+    <div className="container mx-auto flex min-h-screen justify-center bg-green-100 px-4 py-4">
       {isLoading && <Loading />}
       {error && <ErrorOccurred />}
       {!isLoading && !error && teammates?.length === 0 && <NotFound />}
       {!isLoading && !error && teammates?.length !== 0 && (
-        <GridWithHoverEffect>
-          {teammates?.map((teammate: Teammate) => (
-            <>
-              <Button
-                className="absolute top-2 right-2 rounded-full 2xl:top-3 2xl:right-3"
-                variant="secondary"
-                size="syncIcon"
-                onClick={() => handleEdit(teammate)}
-              >
-                <Pencil />
+        <div className="flex w-full flex-col gap-4">
+          <div className="flex w-full items-center justify-between">
+            <span className="text-2xl font-bold">Teams</span>
+            <div className="flex gap-4">
+              <Button variant="outline" size="icon">
+                <Shuffle />
               </Button>
-              <div className="pointer-events-none translate-y-1 p-4 sm:p-3">
-                <Avatar className="aspect-square size-full border">
-                  <AvatarImage
-                    className="bg-secondary object-cover"
-                    src={teammate.avatar || undefined}
-                  />
-                  <AvatarFallback style={{ backgroundColor: teammate.color }}>
-                    {teammate.name.slice(0, 2).toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
-              </div>
-              <div className="flex flex-col text-center">
-                <span className="overflow-hidden text-lg font-semibold text-ellipsis sm:text-2xl">
-                  {teammate.name}
-                </span>
-                <p className="text-muted-foreground overflow-hidden text-xs text-ellipsis sm:text-sm">
-                  {teammate.position}
-                </p>
-              </div>
-            </>
-          ))}
-        </GridWithHoverEffect>
+              <Button variant="outline" size="icon">
+                <Dices />
+              </Button>
+            </div>
+          </div>
+          <GridWithHoverEffect>
+            {teammates?.map((teammate: Teammate) => (
+              <>
+                <Button
+                  className="absolute top-2 right-2 rounded-full 2xl:top-3 2xl:right-3"
+                  variant="secondary"
+                  size="syncIcon"
+                  onClick={() => handleEdit(teammate)}
+                >
+                  <Pencil />
+                </Button>
+                <div className="pointer-events-none translate-y-1 p-4 sm:p-3">
+                  <Avatar className="aspect-square size-full border">
+                    <AvatarImage
+                      className="bg-secondary object-cover"
+                      src={teammate.avatar || undefined}
+                    />
+                    <AvatarFallback style={{ backgroundColor: teammate.color }}>
+                      {teammate.name.slice(0, 2).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                </div>
+                <div className="flex flex-col text-center">
+                  <span className="overflow-hidden text-lg font-semibold text-ellipsis sm:text-2xl">
+                    {teammate.name}
+                  </span>
+                  <p className="text-muted-foreground overflow-hidden text-xs text-ellipsis sm:text-sm">
+                    {teammate.position}
+                  </p>
+                </div>
+              </>
+            ))}
+          </GridWithHoverEffect>
+        </div>
       )}
     </div>
   );
--- src/components/dashboard/teammates/hero.tsx
+++ src/components/dashboard/teammates/hero.tsx
@@ -4,6 +4,12 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { LayoutGrid, Plus } from 'lucide-react';
 import { useTeammatesStore } from '@/store/useTeammatesStore';
+import {
+  Accordion,
+  AccordionTrigger,
+  AccordionContent,
+  AccordionItem,
+} from '@/components/ui/accordion';
 
 const TeammatesHero = () => {
   const { openModal } = useTeammatesStore();
@@ -13,26 +19,40 @@ const TeammatesHero = () => {
   };
 
   return (
-    <div className="container mx-auto flex min-h-screen items-center justify-center px-4">
-      <div className="max-w-2xl p-4 text-center">
-        <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl md:leading-[1.2]">Teammates</h1>
-        <p className="mt-6 text-base md:text-lg">
-          Add, update, or remove teammates and assign them to teams to streamline collaboration.
-          Keep your projects and resources organized by grouping related work together and
-          maintaining a structured, efficient environment for your entire team.
-        </p>
-        <div className="mt-12 flex items-center justify-center gap-4">
-          <Button className="cursor-pointer" onClick={handleCreate}>
-            <Plus /> Create Teammate
-          </Button>
-          <Button variant="outline" size="icon" asChild>
-            <Link href="/teams">
-              <LayoutGrid />
-            </Link>
-          </Button>
-        </div>
-      </div>
-    </div>
+    <Accordion type="single" collapsible className="container mx-auto px-4">
+      <AccordionItem value="item-1">
+        <AccordionTrigger asChild>
+          <div className="flex items-center justify-between py-4">
+            <span className="block text-base font-semibold">Information</span>
+            <Button variant="outline" size="icon">
+              <Plus />
+            </Button>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent>
+          <div className="mx-auto flex min-h-[calc(100dvh-(var(--navbar-height)+56px))] max-w-2xl flex-col items-center justify-center text-center">
+            <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl md:leading-[1.2]">
+              Teammates
+            </h1>
+            <p className="mt-6 text-base md:text-lg">
+              Add, update, or remove teammates and assign them to teams to streamline collaboration.
+              Keep your projects and resources organized by grouping related work together and
+              maintaining a structured, efficient environment for your entire team.
+            </p>
+            <div className="mt-12 flex items-center justify-center gap-4">
+              <Button className="cursor-pointer" onClick={handleCreate}>
+                <Plus /> Create Teammate
+              </Button>
+              <Button variant="outline" size="icon" asChild>
+                <Link href="/teams">
+                  <LayoutGrid />
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
